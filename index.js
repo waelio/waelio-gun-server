70e614df4dec594acd0e2cbc1ff6d70a58529131
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2021-10-02 18:23:23 +0300

    v1 gun server

--- index.js
+++ index.js
@@ -1,29 +1,64 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const { uuid } = require('uuidv4');
+const { Client, Config, CheckoutAPI } = require('@adyen/api-library');
+
+const app = express();
+// setup request logging
+app.use(morgan('dev'));
+// Parse JSON bodies
+app.use(express.json());
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+// Serve client from build folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+// enables environment variables by
+// parsing the .env file and assigning it to process.env
+dotenv.config({
+  path: './.env'
+});
+
+// Adyen Node.js API library boilerplate (configuration, etc.)
+const config = new Config();
+config.apiKey = process.env.API_KEY;
+const client = new Client({ config });
+client.setEnvironment('TEST');
+const checkout = new CheckoutAPI(client);
+
+app.engine(
+  'handlebars',
+  hbs({
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+    helpers: require('./util/helpers')
+  })
+);
+
+app.set('view engine', 'handlebars');
+
+const Gun = require('gun');
+app.use(Gun.serve);
+app.use(express.static(__dirname));
 const port =
   process.env.NODEJS_PORT ||
   process.env.APP_PORT ||
   process.env.PORT ||
   process.argv[2] ||
-  9000;
+  9417;
 
-const express = require('express');
-const app = express();
-const Gun = require('gun');
-app.use(Gun.serve);
-app.use(express.static(__dirname));
-
-const server = app.listen(port, console.log(`Server started on port 9000`));
+const server = app.listen(port, console.log(`Server started on port ${port}`));
 
 const gun = new Gun({
-  file: 'data.json',
   web: server,
   peers: [],
-  mongo: {
-    database: 'gun_waelio',
-    collection: 'cloud_server'
-  },
+  mongo: process.env.MONGO_URL,
   verify: {
     check: (e) => {
-      console.log(`PEER Connected`, e);
+      console.log(`PEER Connected`, e.origin);
       return true;
     }
   }
