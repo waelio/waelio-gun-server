ac6bc513a312d8e6e2c716550b21d622deb1f357
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2021-10-02 22:44:17 +0300

    debug

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ dotenv.config({
   path: './.env'
 });
 
-const White_List = ['http://localhost:9000/gun','https://waelio.herokuapp.com:9000/gun','https://gun.waelio.com:9000/gun'];
+const White_List = [
+  'http://localhost:9000/gun',
+  'https://waelio.herokuapp.com:9000/gun',
+  'https://gun.waelio.com:9000/gun'
+];
 const corsOptions = {
   origin: function (origin, callback) {
     if (White_List.indexOf(origin) !== -1) {
@@ -21,7 +25,11 @@ app.use(cors(corsOptions));
 
 const Gun = require('gun');
 app.use(Gun.serve);
-const port = OPENSHIFT_NODEJS_PORT || process.env.APP_PORT ||  process.env.VCAP_APP_PORT ||  9417;
+const port =
+  OPENSHIFT_NODEJS_PORT ||
+  process.env.APP_PORT ||
+  process.env.VCAP_APP_PORT ||
+  9417;
 const server = app.listen(port, console.log(`Server started on port ${port}`));
 
 const gun = new Gun({
@@ -35,3 +43,8 @@ const gun = new Gun({
     }
   }
 });
+
+global.Gun = Gun; /// make global to `node --inspect` - debug only
+global.gun = gun; /// make global to `node --inspect` - debug only
+
+console.log('Server started on port ' + port + ' with /gun');
