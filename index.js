2b8bf6ed22fdff98c4671a79ae9651c36e73f747
Wael Wahbeh <88880103+peace2074@users.noreply.github.com>
2021-10-02 22:27:52 +0300

    adding cors

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const express = require('express');
-
-
+const cors = require('cors');
 const dotenv = require('dotenv');
-
 const app = express();
 
 dotenv.config({
   path: './.env'
 });
 
+const White_List = ['http://localhost:9000/gun','https://waelio.herokuapp.com:9000/gun','https://gun.waelio.com:9000/gun'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (White_List.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));
+
 const Gun = require('gun');
 app.use(Gun.serve);
-app.use(express.static(__dirname));
 const port =  process.env.APP_PORT ||  9417;
 const server = app.listen(port, console.log(`Server started on port ${port}`));
 
